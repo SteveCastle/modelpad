057918b3b1faf33cc881f8946e76edd31e974c23
Stephen Castle <sc@elf.pub>
2024-09-22 13:01:26 -0700

    add prompt editor to mobile

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -245,7 +245,6 @@ function App() {
               maxWidth: viewSettings.readingMode ? "960px" : "",
               margin: viewSettings.readingMode ? "0 auto" : "0 20px",
               padding: viewSettings.readingMode ? "20px" : "0",
-              height: "100%",
               overflow: "hidden",
               fontSize: `${viewSettings.zoom}em`,
             }}
